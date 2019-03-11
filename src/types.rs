f3f35abee793c7da6bd7e230d72db744c0c9a039
k-nasa <htilcs1115@gmail.com>
2019-03-11 18:27:13 +0900

    feat: Add evel implement

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,5 @@
-use proc_macro2::TokenStream;
+use proc_macro2::{Ident, Span, TokenStream};
+use quote::quote;
 
 #[derive(Clone, Debug)]
 pub struct DescribeStatement {
@@ -25,6 +26,63 @@ impl DescribeStatement {
             subject,
         }
     }
+
+    pub fn expands(describe_statements: Vec<Self>) -> TokenStream {
+        let mut stream = TokenStream::new();
+
+        for statement in describe_statements {
+            let token_stream = statement.expand();
+            stream = stream.into_iter().chain(token_stream.into_iter()).collect();
+        }
+
+        stream
+    }
+
+    // why result?
+    pub fn expand(&self) -> TokenStream {
+        let mod_name = Ident::new(&self.name, Span::call_site());
+        let before = self.before.clone().unwrap_or(TokenStream::new());
+        let after = self.before.clone().unwrap_or(TokenStream::new());
+
+        let subject = if let Some(subject) = self.subject.clone() {
+            quote! {
+                // FIXME want to replace it when evaluated
+                // instead of making it a variable
+                let subject = (#subject);
+            }
+        } else {
+            TokenStream::new()
+        };
+
+        let containers = Self::expand_containers(&self.containers, before, after, subject);
+
+        quote! {
+            mod #mod_name {
+                #containers
+            }
+        }
+    }
+
+    fn expand_containers(
+        containers: &Vec<Container>,
+        before: TokenStream,
+        after: TokenStream,
+        subject: TokenStream,
+    ) -> TokenStream {
+        let mut stream = TokenStream::new();
+
+        for container in containers {
+            let token_stream = match container {
+                Container::Describe(describe) => describe.expand(),
+                Container::Test(test) => {
+                    test.expand(before.clone(), after.clone(), subject.clone())
+                }
+            };
+
+            stream = stream.into_iter().chain(token_stream.into_iter()).collect();
+        }
+        stream
+    }
 }
 
 impl PartialEq for DescribeStatement {
@@ -37,6 +95,30 @@ impl PartialEq for DescribeStatement {
 pub struct Test {
     pub name: String,
     pub container: TokenStream,
+}
+
+impl Test {
+    pub fn expand(
+        &self,
+        before: TokenStream,
+        after: TokenStream,
+        subject: TokenStream,
+    ) -> TokenStream {
+        let test_name = Ident::new(&self.name, Span::call_site());
+        let container = self.container.clone();
+
+        quote! {
+            #[test]
+            fn #test_name() {
+                #before
+                #subject
+
+                #container
+
+                #after
+            }
+        }
+    }
 }
 
 #[derive(Clone, Debug, Default)]
