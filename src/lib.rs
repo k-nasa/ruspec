785d9d4f095838e720b94fe01fcf4652ae77fdda
k-nasa <htilcs1115@gmail.com>
2019-03-11 18:42:34 +0900

    doc: remove comment

--- src/lib.rs
+++ src/lib.rs
@@ -8,48 +8,6 @@ use types::DescribeStatement;
 mod parser;
 mod types;
 
-/// example syntax
-/// ```rust
-/// ruspec! {
-///   describe "test name" {
-///     before {
-///        let context = 5;
-///     }
-///
-///     it "should equal 5" {
-///       assert_eq!(context, 5)
-///     }
-///
-///     describe "inner describe" {
-///       subject { context + 8 }
-///
-///       it "should equal 13" {
-///         assert_eq!(subject, 13)
-///       }
-///     }
-///   }
-/// }
-/// ```
-///
-/// this code is expande it
-///```rust
-/// mod test_name {
-///     #[test]
-///     fn should_equal_5() {
-///         let context = 5;
-///         assert_eq!(context, 5)
-///     }
-///
-///     mod inner_describe {
-///         #[test]
-///         fn should_equal_13() {
-///             let context = 5;
-///             assert_eq!(context + 8, 13)
-///         }
-///     }
-/// }
-///```
-///
 #[proc_macro]
 pub fn ruspec(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = proc_macro2::TokenStream::from(input);
